dddd5931b9af2992b61207aba90554ddd012a578
codescaptain <ahmet-57-@hotmail.com>
2023-12-17 21:12:16 +0300

    main [hot-fix] reponsive style

--- app/services/medium_article_service.rb
+++ app/services/medium_article_service.rb
@@ -22,7 +22,7 @@ class MediumArticleService
 
   def make_svg
     <<-SVG
-  <svg fill="none" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
+  <svg fill="none" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" style="min-width: 500px; min-height: 250px;">
 	<foreignObject width='100%' height='100%'>
     #{style}
 		<div xmlns='http://www.w3.org/1999/xhtml'>
